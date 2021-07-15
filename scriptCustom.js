404250e0feec4043e7b1b2ef03e60d5a5e123a06
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-07-15 20:14:25 +0700

    first-commit

---
+++ scriptCustom.js
@@ -0,0 +1,23 @@
+function goToSimpleTalk() {
+  window.open("https://simple-talk.netlify.app/", "_blank");
+}
+function goToDompetMu() {
+  window.open("https://dompetmu.vercel.app/landing", "_blank");
+}
+function goToCoffeeExpress() {
+  window.open("https://coffee-frontend.vercel.app/", "_blank");
+}
+function goToTiketinAja() {
+  window.open("https://tiketinaja.netlify.app/", "_blank");
+}
+
+document
+  .getElementById("simple-talk")
+  .addEventListener("click", goToSimpleTalk);
+document.getElementById("dompet-mu").addEventListener("click", goToDompetMu);
+document
+  .getElementById("coffee-express")
+  .addEventListener("click", goToCoffeeExpress);
+document
+  .getElementById("tiketin-aja")
+  .addEventListener("click", goToTiketinAja);
